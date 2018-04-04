877c5e137eeea7537c188c45c97c6fce2b4e5c34
dointhedev <oodavis.avi@gmail.com>
2018-04-04 16:34:10 -0700

    Fine tunes .js and file structure for production

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,81 +1,131 @@
-$(document).ready(function () {
-    // Create a function that creates the start button and initial screen
-    jsSetup();
-});
-// ::::::::::: GLOBAL VARIABLES :::::::::
-var buttons = ["dancing", "hugging", "walking", "laughing", "bitching", "caging", "today", "funny", "outdoors"];
+/*::::::DOM CACHE::::::*/
+var clickSound = new Audio("assets/audio/button-click.mp3");
+var audio = document.getElementById('bgAudio');
 var gameCont = $("#gameCont");
-console.log(gameCont);
 
+// ::::::::::: GLOBAL VARIABLES :::::::::
+var topics = ["dancing", "hugging", "walking", "laughing", "bitching", "caging", "today", "funny", "outdoors"];
+var apiKey = "dLKvEKP8ZF6G5KkiIJCHB7vwJOAJbnvr"
+
+// on page load start the application 
+$(document).ready(function () {
+    jsSetup();
+    audio.currentTime = 19.4;
+    audio.play();
+});
+
+// click sound will execute when elements in the body are click on
+$("body").on("click", function (event) {
+    clickSound.play();
+});
+
+// this is the form logic
 function prcesfrm() {
+    // var for the input field 
+    var giphInput = $("#giphName");
+    // when add gif button is pressed
+    $('#giph').submit(function (e) {
+        // prevents the page from reloading
+        e.preventDefault();
+        // resets results 
+        $("#results").remove();
+        // only execute the add gif if form is empty
+        if (giphInput.val().trim() !== "") {
+            // reset buttons
+            $("#buttons").remove();
+            // input value 
+            var newGiph = giphInput.val().trim();
+            // add input value to array
+            topics.push(newGiph);
+            // reset input area 
+            $('#giph').trigger("reset");
+            // call  generate buttons function 
+            genBtns();
+            // call getGiph function
+            getGiph();
+        } else {
+            alert('silly rabbit! you need info in your form to create a button ');
 
+        }
+    });
 
 }
+
 function getGiph() {
-    $("button").on("click", function () {
+// this function is all about gaining access to the API, retrieving the gifs and displaying them in #results
+    $(".start").on("click", function () {
         $("#results").remove();
         var rsltCont = $(" <div id='results'>");
         rsltCont.addClass("col-12 blkBrdr my-3 py-3");
-        console.log(rsltCont);
         $("#gameCont").append(rsltCont);
         var giph = $(this).attr("data-giph");
+        
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            giph + "&api_key=dc6zaTOxFJmzC&limit=10";
-console.log(queryURL);
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-            var results = response.data
+            giph + "&api_key=" + apiKey + "&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data;
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div class='item'>");
                 var rating = results[i].rating;
-                console.log(gifDiv);
                 var p = $("<p>").text("Rating: " + rating);
-                console.log(gifDiv);
                 var image = $("<img>");
-                image.attr("src", results[i].images.fixed_height.url);
+                image.addClass("gif img-fluid");
+                image.attr("src", results[i].images.fixed_height_still.url);
+                image.attr("data-animate", results[i].images.fixed_height.url);
+                image.attr("data-still", results[i].images.fixed_height_still.url);
+                image.attr("data-state", "still");
                 gifDiv.append(p);
-                gifDiv.append(image);
+                gifDiv.prepend(image);
                 $("#results").prepend(gifDiv);
-
             }
-
+            $(".gif").on("click", function () {
+                var state = $(this).attr("data-state");
+                // if gif is on the still state execute block of code if not preform the else block of code
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
         });
     });
 }
 
-
 function genBtns() {
-    for (var i = 0; i < buttons.length; i++) {
-        var element = buttons[i];
+    $("#buttons").empty();
+    var btnDiv = $("<div id='buttons'>");
+    for (var i = 0; i < topics.length; i++) {
+        var element = topics[i];
         var btnG = $("<button>");
         btnG.addClass("btn start btn-outline-light mr-2 mb-2");
         btnG.attr("data-giph", element);
         btnG.text(element);
-        $(gameCont).append(btnG);
+        $(btnDiv).append(btnG);
     }
-
-
+    $(gameCont).append(btnDiv);
 }
 
 function giphContent() {
-    console.log("In giphContent");
     var gameHTML = "<h1 class='text-center text-white'>GET YOUR GIPHY ON!! </h1>" +
-        "<p id='text-area' class='py-1'>Click on any buttons to start Giph'N! If you want to add more buttons use form below. </p>" +
+        "<p id='text-area' class='py-1'>Click on any buttons to start Giph'N! If you want to add more buttons use the form below. </p>" +
         "<nav class='navbar navbar-light d-flex justify-content-center mb-2'>" +
-        "<form id='giph' class='form-inline'>" +
-        "<input id='giphName' class='form-control mr-sm-2' type='search' placeholder='Create Giph' aria-label='Create Giph'>" +
-        "<button id='getGiph' class='btn start btn-outline-light my-2 my-sm-0' type='submit'>Create Giphy</button>" +
+        "<form id='giph' class='form-inline d-flex justify-content-sm-right justify-content-center'>" +
+        "<input id='giphName' class='form-control mr-sm-2' type='text' placeholder='Create Giph' aria-label='Create Giph'>" +
+        "<button id='getGiph' class='btn btn-outline-light my-2 my-sm-0' type='submit'>Create Giphy</button>" +
         "</form>" +
-        "</nav>";
+        "</nav><hr>";
     $(gameCont).html(gameHTML);
-    genBtns()
-    getGiph();
 }
-
-
 
 function jsSetup() {
     giphContent();
+    genBtns();
+    getGiph();
+    prcesfrm();
 }
